d698ecf38f4279f65cf564655ac19306551d7294
raghuram <raghuramreddy.nagireddy@anugrehaa.com>
2017-10-03 17:15:24 +0530

    donw with logs structure

--- api/sale-invoice-service/servicetype.js
+++ api/sale-invoice-service/servicetype.js
@@ -8,16 +8,18 @@ var header=require('../../utils/utils');
 router.get('/:companyId/invoices/', function(req, res) {
 
     var companyId = req.params.companyId;
+    var requrl=config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service?format=json";
     var options = { headers: header,
-        url: config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service?format=json"
+        url: requrl
+
     }
     request.get(options, function(error, response, body) {
         res.set('Content-Type', 'Application/json');
         if (!error && response.statusCode == 200) {
-            log.info({respose:body},response.statusCode);
+            log.info({request_url:requrl,response_body:body,status_code:response.statusCode},"Request for list of invoices");
             res.status(response.statusCode).send(body);
         } else {
-            log.error({respose:body},response.statusCode);
+            log.error({response_body:body,request_url:requrl,status_code:response.statusCode},"Request for list of invoice");
             res.status(response.statusCode).send(body);
 
         }
@@ -28,16 +30,18 @@ router.get('/:companyId/invoices/', function(req, res) {
 router.get('/:companyId/invoices/:id', function(req, res) {
     var companyId = req.params.companyId;
     var id = req.params.id;
+    var requrl=config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service/"+id+"?format=json"
+
     var options = { headers: header,
-     url: config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service/"+id+"?format=json"
+     url: requrl
     }
     request.get(options, function(error, response, body) {
         res.set('Content-Type', 'Application/json');
         if (!error && response.statusCode == 200) {
-          log.info({respose:body},response.statusCode);
+         log.info({request_url:requrl,response_body:body,status_code:response.statusCode},"Request for single invoice");
             res.status(response.statusCode).send(body);
         } else {
-           log.error({respose:body},response.statusCode);
+           log.error({response_body:body,request_url:requrl,status_code:response.statusCode},"Request for single invoice");
           //  console.log("failure response from Myob: "+body);
             res.status(response.statusCode).send(body);
         }
@@ -48,17 +52,19 @@ router.get('/:companyId/invoices/:id', function(req, res) {
 router.post('/:companyId/invoices',function(req, res) {
     var companyId = req.params.companyId;
     var requestBody = JSON.stringify(req.body);
+    var requrl=config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service?format=json"
     var options = { headers:header,
-        url: config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service?format=json",
+        url: requrl,
         body: requestBody
     }
     request.post(options, function(error, response, body) {
         res.set('Content-Type', 'Application/json');
         if (!error && response.statusCode == 201) {
+         log.info({request_url:requrl,request_body:requestBody,status_code:response.statusCode},"creating single invoice");
             res.status(response.statusCode).send(body);
-            log.info({respose:body},response.statusCode);
+
       } else {
-           log.error({respose:body},response.statusCode);
+           log.error({response_body:body,request_body:requestBody,request_url:requrl,status_code:response.statusCode},"Request to create invoice");
            res.status(response.statusCode).send(body);
 
         }
@@ -68,19 +74,19 @@ router.post('/:companyId/invoices',function(req, res) {
 router.put('/:companyId/invoices/:id', function(req, res) {
     var id = req.params.id;
     var companyId = req.params.companyId;
+    var requrl=config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service/"+id+"/?format=json";
     var requestBody = JSON.stringify(req.body);
     var options = { headers:header,
-        url: config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service/"+id+"/?format=json",
+        url: requrl,
         body: requestBody
     }
     request.post(options, function(error, response, body) {
         res.set('Content-Type', 'Application/json');
         if (!error && response.statusCode == 200) {
-          log.info({respose:body},response.statusCode);
+          log.info({request_url:requrl,request_body:requestBody,status_code:response.statusCode},"Updating a single invoice");
             res.status(response.statusCode).send(body);
         } else {
-            log.error({respose:body},response.statusCode);
-            //console.log("failure response from Myob: "+body);
+         log.error({response_body:body,request_body:requestBody,request_url:requrl,status_code:response.statusCode},"updating a single invoice");
             res.status(response.statusCode).send(body);
         }
     });
@@ -89,19 +95,19 @@ router.put('/:companyId/invoices/:id', function(req, res) {
 router.delete('/:companyId/invoices/:id', function(req, res) {
     var id = req.params.id;
     var companyId = req.params.companyId;
-    var requestBody = JSON.stringify(req.body);
+    var requrl=config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service/"+id+"/?format=json"
+
     var options = { headers:header,
-        url: config.get('myob.host') +"/AccountRight/"+companyId+"/Sale/Invoice/Service/"+id+"/?format=json",
+        url: requrl,
         body: requestBody
     }
     request.post(options, function(error, response, body) {
         res.set('Content-Type', 'Application/json');
         if (!error && response.statusCode == 200) {
-            log.info({respose:body},response.statusCode);
+           log.info({request_url:requrl,status_code:response.statusCode},"Deleting a single invoice");
             res.status(response.statusCode).send(body);
         } else {
-            log.error({respose:body},response.statusCode);
-            //console.log("failure response from Myob: "+body);
+           log.error({request_url:requrl,status_code:response.statusCode},"Deleting a single invoice");
             res.status(response.statusCode).send(body);
         }
     });
