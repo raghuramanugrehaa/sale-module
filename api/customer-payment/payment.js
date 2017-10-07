857df35004dd37761693fa73da3716a33cec0338
starkshouse <starkshouse.gopi@gmail.com>
2017-10-07 12:45:17 +1100

    Added log messages to payments flow

--- api/customer-payment/payment.js
+++ api/customer-payment/payment.js
@@ -8,21 +8,25 @@ var header=require('../../utils/utils');
 // gets the list of all the invoice that are having status opened of a company
 
 router.get('/:companyId/customerPayments', function(req, res) {
+
     var companyId = req.params.companyId;
+    log.info("Request received for retrieve all customerPayments records with companyId: "+companyId);
     var options = { headers:header,
         url: config.get('myob.host') +"/AccountRight/"+companyId+"/sale/CustomerPayment?format=json"
     }
+    log.info("Request dispatched to MYOB for retrieve all customerPayments records with request method: GET, url: "+options.url+ " and body: "+ null);
 
     request.get(options, function(error, response, body) {
         res.set('Content-Type', 'Application/json');
+        log.info("Response received from MYOB for retrieve all customerPayments records with statusCode: "+response.statusCode+ " and body: "+ body);
         if (!error && response.statusCode == 200) {
-            log.info({response:body},response.statusCode);
+            log.debug("success response received from MYOB");
             res.status(response.statusCode).send(body);
         } else {
-            log.error({respose:body},response.statusCode);
+            log.error("failure response received from MYOB");
             res.status(response.statusCode).send(body);
-
         }
+        log.info("Response dispatched for retrieve all customerPayments records with companyId: "+companyId + ", statusCode: "+ response.statusCode + " and response: "+ body);
     });
 })
 
